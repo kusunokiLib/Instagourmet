ac62ff703a414fbb44228a754ef3854663d80740
r-yanyo <honma.ryosuke@gmail.com>
2018-04-28 15:17:07 +0900

    fix typo

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   belongs_to :user
-  has_many :favirites
-  has_many :user, :through => :favirites
+  has_many :favorites
+  has_many :user, :through => :favorites
 end
