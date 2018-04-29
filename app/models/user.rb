3097e16bc718bf0feb8781ab3b39053d1f0f4494
r-yanyo <honma.ryosuke@gmail.com>
2018-04-30 02:11:37 +0900

    Implement login & post

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,10 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  def new
+    
+  end
+
   # Postをfavoriteする
   def favorite(post)
     favorites.create(post_id: post.id)
