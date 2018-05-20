1cfc2344ca20553feace94569a870e386616a481
r-yanyo <honma.ryosuke@gmail.com>
2018-05-20 17:07:21 +0900

    Allow get method from anywhare

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,8 @@ module Instagourmet
 
     config.action_dispatch.default_headers = {
       'Access-Control-Allow-Credentials' => 'true',
-      'Access-Control-Allow-Origin' => 'http://localhost:8081',
-      'Access-Control-Request-Method' => '*'
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => 'get'
     }
   end
 end
